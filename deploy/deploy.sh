73610203a53b9d6f352c5fa3a682b70e73a7d056
Aleksandr Glebov <89081349143@mail.ru>
2024-05-01 19:19:14 -0600

    test again 5

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,6 +10,8 @@ docker rmi aleksglebov/licensure:back || true
 # Получить последнюю версию образа из Docker Hub
 docker pull aleksglebov/licensure:back
 
+set -o allexport; source /etc/environment; set +o allexport
+
 # Запустить новый контейнер
 docker run -p 8082:8082 -d --name back -e SECRET_KEY="$SECRET_KEY" aleksglebov/licensure:back
 
